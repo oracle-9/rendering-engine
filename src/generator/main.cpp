0ebcf50643a71464e7ff24dc42390ffbe0a05894
oracle <oracle-main@pm.me>
2022-03-07 15:20:01 +0000

    Format generator/main.cpp.

--- src/generator/main.cpp
+++ src/generator/main.cpp
@@ -129,7 +129,10 @@ auto parse_u32(char const* const s) -> u32 {
     return uninit;
 }
 
-auto check_num_args(std::size_t const expected, std::size_t const actual) -> void {
+auto check_num_args(
+    std::size_t const expected,
+    std::size_t const actual
+) -> void {
     if (expected != actual) {
         throw std::invalid_argument {
             fmt::format("expected {} arguments, but got {}", expected, actual)
