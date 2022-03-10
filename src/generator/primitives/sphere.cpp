68ea0a9bb22aabadabfffee0fa3fa52299eb6ded
Campos <brunitocampos@gmail.com>
2022-03-10 17:11:55 +0000

    sphere comments

--- src/generator/primitives/sphere.cpp
+++ src/generator/primitives/sphere.cpp
@@ -9,8 +9,8 @@ auto generate_sphere(
     fmt::ostream& output_file
 ) -> void {
 
-    float rotationSlice = (float) 2 * M_PI / num_slices;
-	float rotationStack = (float) M_PI / num_stacks;
+    float rotationSlice = (float) 2 * M_PI / num_slices; // 360 / nº slices
+	float rotationStack = (float) M_PI / num_stacks;     // 180 / nº stacks
 
 	// x= r * cos(beta)  * sin(alpha);
 	// y= r * sin(beta);
@@ -23,25 +23,25 @@ auto generate_sphere(
 	for (int i = 0; i < (int) num_stacks; i++) {
 
 		float stackAngle = (float) (M_PI / 2) - i * rotationStack; // beta
-		float stackAngle2 = (float) (M_PI / 2) - (i+1) * rotationStack; // beta de baixo
+		float stackAngle2 = (float) (M_PI / 2) - (i+1) * rotationStack; // beta da stack baixo
 
 		for (int j = 0; j < (int) num_slices; j++) {
 
 			float sliceAngle = j * rotationSlice; // alpha
-			float sliceAngle2 = (j+1) * rotationSlice; // alpha do lado
+			float sliceAngle2 = (j+1) * rotationSlice; // alpha da slice lado
 
-			if (i < num_stacks-1) {
+			if (i < num_stacks-1) { // todas as stacks menos a ultima de baixo
             	output_file.print("{} {} {}\n",radius * cos(stackAngle) * sin(sliceAngle), radius * sin(stackAngle), radius * cos(stackAngle) * cos(sliceAngle));
             	output_file.print("{} {} {}\n",radius * cos(stackAngle2) * sin(sliceAngle), radius * sin(stackAngle2), radius * cos(stackAngle2) * cos(sliceAngle));
             	output_file.print("{} {} {}\n",radius * cos(stackAngle2) * sin(sliceAngle2), radius * sin(stackAngle2), radius * cos(stackAngle2) * cos(sliceAngle2));
 			}
-			else{
+			else{ // stack final separada porque está ao contrario
 				output_file.print("{} {} {}\n",radius * cos(stackAngle2) * sin(sliceAngle2), radius * sin(stackAngle2), radius * cos(stackAngle2) * cos(sliceAngle2));
                 output_file.print("{} {} {}\n",radius * cos(stackAngle) * sin(sliceAngle2), radius * sin(stackAngle), radius * cos(stackAngle) * cos(sliceAngle2));
                 output_file.print("{} {} {}\n",radius * cos(stackAngle) * sin(sliceAngle), radius * sin(stackAngle), radius * cos(stackAngle) * cos(sliceAngle));
 			}
 
-			if (i > 0 && i < (int) num_stacks-1) {
+			if (i > 0 && i < (int) num_stacks-1) {  // só as stacks que têm 2 triangulos por face
                 output_file.print("{} {} {}\n",radius * cos(stackAngle2) * sin(sliceAngle2), radius * sin(stackAngle2), radius * cos(stackAngle2) * cos(sliceAngle2));
                 output_file.print("{} {} {}\n",radius * cos(stackAngle) * sin(sliceAngle2), radius * sin(stackAngle), radius * cos(stackAngle) * cos(sliceAngle2));
                 output_file.print("{} {} {}\n",radius * cos(stackAngle) * sin(sliceAngle), radius * sin(stackAngle), radius * cos(stackAngle) * cos(sliceAngle));
