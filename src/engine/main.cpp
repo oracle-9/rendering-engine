a49197c9d9cc78a14fcc6308fd2795ea70e58fbd
oracle <oracle-main@pm.me>
2022-03-11 11:51:17 +0000

    Use pretty print in engine/main.cpp.

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -1,15 +1,21 @@
 #include "engine/module.hpp"
+#include "util/pretty_print.hpp"
 
 #include <cstdlib>
 #include <fmt/core.h>
+#include <string_view>
+
+using namespace std::string_view_literals;
+
+auto constexpr prog_name = "engine"sv;
 
 auto main() -> int {
     auto world_result
         = engine::parse::xml::parse_world("engine_input/world.xml");
 
     if (world_result.has_error()) {
-        fmt::print(
-            "failed world generation with error {}\n",
+        pretty_print_err(
+            "failed world generation with error '{}'\n",
             world_result.error()
         );
         return EXIT_FAILURE;
