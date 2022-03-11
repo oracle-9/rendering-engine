7c77fdd69715feac1f6fb23bdcde860e2eb09a22
oracle <oracle-main@pm.me>
2022-03-11 11:28:23 +0000

    Implement engine/render/main.cpp.

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -1,1 +1,19 @@
-auto main() -> int {}
+#include "engine/module.hpp"
+
+#include <cstdlib>
+#include <fmt/core.h>
+
+auto main() -> int {
+    auto world_result
+        = engine::parse::xml::parse_world("engine_input/world.xml");
+
+    if (world_result.has_error()) {
+        fmt::print(
+            "failed world generation with error {}\n",
+            world_result.error()
+        );
+        return EXIT_FAILURE;
+    }
+
+    (*world_result).render();
+}
