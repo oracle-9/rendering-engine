250046c5f33d473ee0050973028fdfd46ba682e8
oracle <oracle-main@pm.me>
2022-03-27 16:44:57 +0100

    Use inline global variables in engine/config.hpp to prevent variable duplication.

--- include/engine/config.hpp
+++ include/engine/config.hpp
@@ -10,12 +10,14 @@
 namespace engine {
 
 namespace config {
-    auto constexpr prog_name = std::string_view{"engine"};
+
+auto inline constexpr prog_name = std::string_view{"engine"};
+
 } // namespace config
 
 namespace render::config {
 
-char constexpr WIN_TITLE[] = "engine";
+char inline constexpr WIN_TITLE[] = "engine";
 
 enum : int {
     DEFAULT_WIN_POS_X = 100,
@@ -24,6 +26,7 @@ enum : int {
     DEFAULT_WIN_WIDTH = 800,
     DEFAULT_WIN_HEIGHT = 800,
 };
+
 static_assert(
     DEFAULT_WIN_HEIGHT > 0,
     "DEFAULT_WIN_HEIGHT must be greater than zero to prevent divide by zero"
@@ -34,14 +37,15 @@ auto constexpr DEFAULT_ASPECT_RATIO
     = static_cast<double>(DEFAULT_WIN_WIDTH)
     / static_cast<double>(DEFAULT_WIN_WIDTH);
 
-auto constexpr DEFAULT_BG_COLOR = glm::vec4 {
+auto inline constexpr DEFAULT_BG_COLOR = glm::vec4 {
     0.08f, // R
     0.08f, // G
     0.08f, // B
     1.f,   // A
 };
 
-auto constexpr DEFAULT_FG_COLOR = glm::vec4{1.f, 1.f, 1.f, 1.f}; // white
+ // white
+ auto inline constexpr DEFAULT_FG_COLOR = glm::vec4{1.f, 1.f, 1.f, 1.f};
 
 enum : GLenum { DEFAULT_POLYGON_MODE = GL_LINE };
 
@@ -53,18 +57,17 @@ enum kb_keys : unsigned char {
     KEY_NEXT_POLYGON_MODE = 'm',
 };
 
-auto constexpr Y_AXIS_HALF_LEN = 50.f;
-auto constexpr Z_AXIS_HALF_LEN = 50.f;
-auto constexpr X_AXIS_HALF_LEN = 50.f;
+auto inline constexpr Y_AXIS_HALF_LEN = 50.f;
+auto inline constexpr Z_AXIS_HALF_LEN = 50.f;
+auto inline constexpr X_AXIS_HALF_LEN = 50.f;
 
-auto constexpr AXIS_COLOR = std::to_array<glm::vec3>({
+auto inline constexpr AXIS_COLOR = std::to_array<glm::vec3>({
     {1.f, 0.f, 0.f}, // x axis (red)
     {0.f, 1.f, 0.f}, // y axis (green)
     {0.f, 0.f, 1.f}, // z axis (blue)
 });
 
-
-auto const DEFAULT_WORLD = world {
+auto inline const DEFAULT_WORLD = world {
     .camera = {
         .pos = {9., 3., 30.},
         .lookat = {0., 0., 0.},
@@ -76,7 +79,7 @@ auto const DEFAULT_WORLD = world {
 
 enum : unsigned int { RENDER_TICK_MILLIS = 16 }; // 60 FPS
 
-auto constexpr CAM_TRANSL_FACTOR = 0.06;
+auto inline constexpr CAM_TRANSL_FACTOR = 0.06;
 
 } // namespace render::config
 
