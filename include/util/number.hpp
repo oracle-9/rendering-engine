3a0e013fd5cf9d6451ce4daf254808ef336ebf57
oracle <oracle-main@pm.me>
2022-03-27 13:56:43 +0100

    Use alternative operator representations.

--- include/util/number.hpp
+++ include/util/number.hpp
@@ -5,6 +5,6 @@
 namespace util {
 
 template <typename T>
-concept number = std::integral<T> || std::floating_point<T>;
+concept number = std::integral<T> or std::floating_point<T>;
 
 } // namespace util
