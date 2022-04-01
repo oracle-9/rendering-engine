cd9f8700afebb4186f00350e50f5f51d719ee280
oracle <oracle-main@pm.me>
2022-04-02 00:07:14 +0100

    Add lib nonnull_ptr.

---
+++ lib/nonnull_ptr/nonnull_ptr.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <cassert>
+#include <cstddef>
+#include <memory>
+#include <type_traits>
+
+namespace nonnull_ptr {
+
+template <typename T>
+    requires (not std::is_reference_v<T>)
+class nonnull_ptr {
+  private:
+    T* ptr;
+
+  public:
+    nonnull_ptr() = delete;
+
+    nonnull_ptr(std::nullptr_t) = delete;
+
+    constexpr nonnull_ptr(T* const ptr) noexcept
+        : ptr{[ptr] {
+            assert(ptr != nullptr);
+            return ptr;
+        }()}
+    {}
+
+    nonnull_ptr(nonnull_ptr const&) = default;
+
+    nonnull_ptr(nonnull_ptr&&) = default;
+
+    auto operator=(nonnull_ptr const&) -> nonnull_ptr& = default;
+
+    auto operator=(nonnull_ptr&&) -> nonnull_ptr& = default;
+
+    ~nonnull_ptr() = default;
+
+    [[nodiscard]]
+    constexpr operator T*() noexcept {
+        return this->ptr;
+    }
+
+    [[nodiscard]]
+    constexpr operator T&() noexcept {
+        return *this->ptr;
+    }
+};
+
+template <typename T>
+[[nodiscard]]
+auto nonnull_ptr_to(T& val) noexcept -> nonnull_ptr<T> {
+    return nonnull_ptr{std::addressof(val)};
+}
+
+} // namespace nonnull_ptr
