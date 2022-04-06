6e1af53676c6e020bcd8e7a76330ff216e9e16cc
oracle <oracle-main@pm.me>
2022-04-06 18:57:15 +0100

    Replace intrinsics::branching long type requirement with bool.

--- lib/intrinsics/branching.hpp
+++ lib/intrinsics/branching.hpp
@@ -7,20 +7,20 @@
 
 namespace intrinsics {
 
-template <std::convertible_to<long> Expr>
+template <std::convertible_to<bool> Expr>
 auto constexpr likely(Expr&& expr)
-    noexcept(std::is_nothrow_convertible_v<Expr, long>)
+    noexcept(std::is_nothrow_convertible_v<Expr, bool>)
 -> bool
 {
-    return __builtin_expect(static_cast<long>(std::forward<Expr>(expr)), 1l);
+    return __builtin_expect(static_cast<bool>(std::forward<Expr>(expr)), 1l);
 }
 
-template <std::convertible_to<long> Expr>
+template <std::convertible_to<bool> Expr>
 auto constexpr unlikely(Expr&& expr)
-    noexcept(std::is_nothrow_convertible_v<Expr, long>)
+    noexcept(std::is_nothrow_convertible_v<Expr, bool>)
 -> bool
 {
-    return __builtin_expect(static_cast<long>(std::forward<Expr>(expr)), 0l);
+    return __builtin_expect(static_cast<bool>(std::forward<Expr>(expr)), 0l);
 }
 
 [[noreturn]]
