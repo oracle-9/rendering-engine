4db518de6ddeea9a441e69d7e59a864eb1580837
oracle <oracle-main@pm.me>
2022-04-16 19:23:04 +0100

    Improve try macros.

--- include/util/try.hpp
+++ include/util/try.hpp
@@ -19,7 +19,7 @@
 })
 
 #define TRY_OPTION(option_expr) ({                                             \
-    auto _option_val = option_expr;                                            \
+    auto&& _option_val = option_expr;                                          \
     if (not _option_val.has_value()) {                                         \
         return {};                                                             \
     }                                                                          \
@@ -27,7 +27,7 @@
 })
 
 #define TRY_OPTION_OR(option_expr, stmt) ({                                    \
-    auto _option_val = option_expr;                                            \
+    auto&& _option_val = option_expr;                                          \
     if (not _option_val.has_value()) {                                         \
         stmt;                                                                  \
     }                                                                          \
@@ -35,7 +35,7 @@
 })
 
 #define TRY_RESULT(result_expr) ({                                             \
-    auto _result_val = result_expr;                                            \
+    auto&& _result_val = result_expr;                                          \
     if (_result_val.has_error()) {                                             \
         return cpp::fail(_result_val.error());                                 \
     }                                                                          \
@@ -43,7 +43,7 @@
 })
 
 #define TRY_RESULT_OR(result_expr, stmt) ({                                    \
-    auto _result_val = result_expr;                                            \
+    auto&& _result_val = result_expr;                                          \
     if (_result_val.has_error()) {                                             \
         stmt;                                                                  \
     }                                                                          \
