406a5c58d05b10bed27a730f3d33eff3649a2837
oracle <oracle-main@pm.me>
2022-04-16 19:36:50 +0100

    Operator syntax consistency fix.

--- include/util/parse_number.hpp
+++ include/util/parse_number.hpp
@@ -17,7 +17,7 @@ auto constexpr parse_number(std::string_view const s) noexcept
     char const* const begin = s.data();
     char const* const end = begin + s.length();
     auto const [parse_end, err] = std::from_chars(begin, end, uninit);
-    if (parse_end != end || err != std::errc{}) {
+    if (parse_end != end or err != std::errc{}) {
         return {};
     }
     return uninit;
