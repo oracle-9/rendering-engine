28eb610d0561ab1be3aadba819cc00db1edc188f
Campos <brunitocampos@gmail.com>
2022-05-02 08:04:34 +0100

    sphere normals and texture coordinates (commented)

--- src/generator/primitives/sphere.cpp
+++ src/generator/primitives/sphere.cpp
@@ -11,6 +11,18 @@ namespace generator {
 
 using namespace brief_int;
 
+/*
+void normalize(float *a) {
+
+    float l = sqrt(a[0]*a[0] + a[1] * a[1] + a[2] * a[2]);
+    a[0] = a[0]/l;
+    a[1] = a[1]/l;
+    a[2] = a[2]/l;
+}
+
+auto normals = std::vector<glm::vec3>{};
+auto texcoord = std::vector<glm::vec2>{};
+*/
 auto generate_sphere(
     float const radius,
     u32 const num_slices,
@@ -29,6 +41,8 @@ try {
         return cpp::fail(GeneratorErr::SPHERE_LT_TWO_STACKS);
     }
 
+    //float tmp0[3],tmp1[3],tmp2[3];
+
     auto const slice_angle
         = glm::pi<float>() * (2.f / static_cast<float>(num_slices));
 
@@ -42,6 +56,10 @@ try {
 
     auto vertices = std::vector<glm::vec3>{};
     vertices.reserve(total_vertex_count);
+    /*
+    normals.reserve(total_vertex_count);
+    texcoord.reserve(total_vertex_count);
+    */
 
     for (auto i = 0_u32; i < num_stacks; ++i) {
         auto static constexpr half_pi = glm::pi<float>() / 2.f;
@@ -78,6 +96,37 @@ try {
                     radius * sin(next_stack_angle),
                     radius * cos(next_stack_angle) * cos(next_slice_angle)
                 );
+                /*
+                tmp0[0] = radius * cos(curr_stack_angle) * sin(curr_slice_angle);
+                tmp0[1] = radius * sin(curr_stack_angle);
+                tmp0[2] = radius * cos(curr_stack_angle) * cos(curr_slice_angle);
+
+                tmp1[0] = radius * cos(next_stack_angle) * sin(curr_slice_angle);
+                tmp1[1] = radius * sin(next_stack_angle);
+                tmp1[2] = radius * cos(next_stack_angle) * cos(curr_slice_angle);
+
+                tmp2[0] = radius * cos(next_stack_angle) * sin(next_slice_angle);
+                tmp2[1] = radius * sin(next_stack_angle);
+                tmp2[2] = radius * cos(next_stack_angle) * cos(next_slice_angle);
+
+                normalize(tmp0);normalize(tmp1);normalize(tmp2);
+                normals.emplace_back(tmp0[0],tmp0[1],tmp0[2]);
+                normals.emplace_back(tmp1[0],tmp1[1],tmp1[2]);
+                normals.emplace_back(tmp2[0],tmp2[1],tmp2[2]);
+                
+                texcoord.emplace_back(
+                    (curr_slice_angle * 180.0f / M_PI) / 360.0f,
+                    ((curr_stack_angle * 180.0f / M_PI) + 90.f) / 180.0f
+                );
+                texcoord.emplace_back(
+                    (curr_slice_angle * 180.0f / M_PI) / 360.0f,
+                    ((next_stack_angle * 180.0f / M_PI) + 90.f) / 180.0f
+                );
+                texcoord.emplace_back(
+                    (next_slice_angle * 180.0f / M_PI) / 360.0f,
+                    ((next_stack_angle * 180.0f / M_PI) + 90.f) / 180.0f
+                );
+                */
             } else {
                 vertices.emplace_back(
                     radius * cos(next_stack_angle) * sin(next_slice_angle),
@@ -94,6 +143,37 @@ try {
                     radius * sin(curr_stack_angle),
                     radius * cos(curr_stack_angle) * cos(curr_slice_angle)
                 );
+                /*
+                tmp0[0] = radius * cos(next_stack_angle) * sin(next_slice_angle);
+                tmp0[1] = radius * sin(next_stack_angle);
+                tmp0[2] = radius * cos(next_stack_angle) * cos(next_slice_angle);
+
+                tmp1[0] = radius * cos(curr_stack_angle) * sin(next_slice_angle);
+                tmp1[1] = radius * sin(curr_stack_angle);
+                tmp1[2] = radius * cos(curr_stack_angle) * cos(next_slice_angle);
+
+                tmp2[0] = radius * cos(curr_stack_angle) * sin(curr_slice_angle);
+                tmp2[1] = radius * sin(curr_stack_angle);
+                tmp2[2] = radius * cos(curr_stack_angle) * cos(curr_slice_angle);
+                
+                normalize(tmp0);normalize(tmp1);normalize(tmp2);
+                normals.emplace_back(tmp0[0],tmp0[1],tmp0[2]);
+                normals.emplace_back(tmp1[0],tmp1[1],tmp1[2]);
+                normals.emplace_back(tmp2[0],tmp2[1],tmp2[2]);
+                
+                texcoord.emplace_back(
+                    (next_slice_angle * 180.0f / M_PI) / 360.0f,
+                    ((next_stack_angle * 180.0f / M_PI) + 90.f) / 180.0f
+                );
+                texcoord.emplace_back(
+                    (next_slice_angle * 180.0f / M_PI) / 360.0f,
+                    ((curr_stack_angle * 180.0f / M_PI) + 90.f) / 180.0f
+                );
+                texcoord.emplace_back(
+                    (curr_slice_angle * 180.0f / M_PI) / 360.0f,
+                    ((curr_stack_angle * 180.0f / M_PI) + 90.f) / 180.0f
+                );
+                */
             }
 
             if (i > 0 && i < num_stacks - 1) {
@@ -112,6 +192,37 @@ try {
                     radius * sin(curr_stack_angle),
                     radius * cos(curr_stack_angle) * cos(curr_slice_angle)
                 );
+                /*
+                tmp0[0] = radius * cos(next_stack_angle) * sin(next_slice_angle);
+                tmp0[1] = radius * sin(next_stack_angle);
+                tmp0[2] = radius * cos(next_stack_angle) * cos(next_slice_angle);
+
+                tmp1[0] = radius * cos(curr_stack_angle) * sin(next_slice_angle);
+                tmp1[1] = radius * sin(curr_stack_angle);
+                tmp1[2] = radius * cos(curr_stack_angle) * cos(next_slice_angle);
+
+                tmp2[0] = radius * cos(curr_stack_angle) * sin(curr_slice_angle);
+                tmp2[1] = radius * sin(curr_stack_angle);
+                tmp2[2] = radius * cos(curr_stack_angle) * cos(curr_slice_angle);
+                
+                normalize(tmp0);normalize(tmp1);normalize(tmp2);
+                normals.emplace_back(tmp0[0],tmp0[1],tmp0[2]);
+                normals.emplace_back(tmp1[0],tmp1[1],tmp1[2]);
+                normals.emplace_back(tmp2[0],tmp2[1],tmp2[2]);
+                
+                texcoord.emplace_back(
+                    (next_slice_angle * 180.0f / M_PI) / 360.0f,
+                    ((next_stack_angle * 180.0f / M_PI) + 90.f) / 180.0f
+                );
+                texcoord.emplace_back(
+                    (next_slice_angle * 180.0f / M_PI) / 360.0f,
+                    ((curr_stack_angle * 180.0f / M_PI) + 90.f) / 180.0f
+                );
+                texcoord.emplace_back(
+                    (curr_slice_angle * 180.0f / M_PI) / 360.0f,
+                    ((curr_stack_angle * 180.0f / M_PI) + 90.f) / 180.0f
+                );
+                */
             }
         }
     }
@@ -138,6 +249,16 @@ try {
     for (auto const& vertex : vertices) {
         output_file.print("{} {} {}\n", vertex.x, vertex.y, vertex.z);
     }
+    /*
+    output_file.print("\n");
+    for (auto const& vertex : normals) {
+        output_file.print("{} {} {}\n", vertex.x, vertex.y, vertex.z);
+    }
+    output_file.print("\n");
+    for (auto const& vertex : texcoord) {
+        output_file.print("{} {}\n", vertex.x, vertex.y);
+    }
+    */
     return {};
 } catch (...) {
     return cpp::fail(GeneratorErr::IO_ERR);
