2a8edd019aea6018f588e1da8cd6941ca59488d7
Campos <brunitocampos@gmail.com>
2022-06-03 21:53:22 +0100

    minifix bezier

--- src/generator/primitives/bezier_patch.cpp
+++ src/generator/primitives/bezier_patch.cpp
@@ -53,6 +53,15 @@ void cross(float *a, float *b, float *res) {
     res[1] = a[2]*b[0] - a[0]*b[2];
     res[2] = a[0]*b[1] - a[1]*b[0];
 }
+
+void normalize(float *a) {
+
+    float l = sqrt(a[0]*a[0] + a[1] * a[1] + a[2] * a[2]);
+    a[0] = a[0]/l;
+    a[1] = a[1]/l;
+    a[2] = a[2]/l;
+}
+
 */
 glm::vec3 calcBenzierCurve(float t, glm::vec3 p0, glm::vec3 p1, glm::vec3 p2, glm::vec3 p3) {
     glm::vec3 res;
@@ -99,6 +108,7 @@ void derivsBenzierCurve(float u, float v, float* x, float* y, float* z, float* n
     realv[2] = vecvz[0]*dV[0] + vecvz[1]*dV[1] + vecvz[2]*dV[2] + vecvz[3]*dV[3];
 
     cross(realu,realv,n);
+    normalize(n);
 }
 */
 auto static parse_patch_file(std::ifstream& patch_file) noexcept
