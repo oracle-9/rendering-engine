dffa734928da92f0c59aae88bd60144b9562e495
Ricardo Gomes <61480049+srcast@users.noreply.github.com>
2022-06-05 13:19:18 +0100

    Update plane.cpp

--- src/generator/primitives/plane.cpp
+++ src/generator/primitives/plane.cpp
@@ -44,10 +44,16 @@ try {
     //   * The generated triangles follow the CCW (counter-clockwise)
     //     convention.
     auto vertices = std::vector<glm::vec3>{};
+    auto normals = std::vector<glm::vec3>{};
+    auto texcoord = std::vector<glm::vec2>{};
     vertices.reserve(total_vertex_count);
+    normals.reserve(total_vertex_count);
+    texcoord.reserve(total_vertex_count);
 
     // Stores the side length of a plane division.
     auto const div_side_len = side_len / static_cast<float>(num_divs);
+
+    auto const div_text_coord = 1 / static_cast<float>(num_divs);
 
     // We iterate division by division.
     // The outer loop traverses the z axis and the inner the loop traverses the
@@ -56,29 +62,48 @@ try {
         // Stores the lower (with lower magnitude, not positionally below) z
         // coordinate of the current division.
         auto const lo_z = static_cast<float>(row) * div_side_len;
+        auto const lo_y_text_coord = static_cast<float>(num_divs - row) * div_text_coord;
 
         // Stores the upper (with higher magnitude, not positionally above) z
         // coordinate of the current division.
         auto const hi_z = static_cast<float>(row + 1) * div_side_len;
+        auto const hi_y_text_coord = static_cast<float>(num_divs - row - 1) * div_text_coord;
 
         for (auto col = 0_u32; col < num_divs; ++col) {
             // Stores the lower (with lower magnitude, not positionally below) x
             // coordinate of the current division.
             auto const lo_x = static_cast<float>(col) * div_side_len;
+            auto const lo_x_text_coord = static_cast<float>(col) * div_text_coord;
 
             // Stores the upper (with higher magnitude, not positionally above)
             // x coordinate of the current division.
             auto const hi_x = static_cast<float>(col + 1) * div_side_len;
+            auto const hi_x_text_coord = static_cast<float>(col + 1) * div_text_coord;
 
             // First we generate the first half of the division.
             vertices.emplace_back(lo_x, 0.f, lo_z);
             vertices.emplace_back(lo_x, 0.f, hi_z);
             vertices.emplace_back(hi_x, 0.f, lo_z);
+            normals.emplace_back(0.f, 1.f, 0.f);
+            normals.emplace_back(0.f, 1.f, 0.f);
+            normals.emplace_back(0.f, 1.f, 0.f);
+
+            texcoord.emplace_back(lo_x_text_coord, lo_y_text_coord);
+            texcoord.emplace_back(lo_x_text_coord, hi_y_text_coord);
+            texcoord.emplace_back(hi_x_text_coord, lo_y_text_coord);
+            
 
             // Then we generate the second.
             vertices.emplace_back(hi_x, 0.f, lo_z);
             vertices.emplace_back(lo_x, 0.f, hi_z);
             vertices.emplace_back(hi_x, 0.f, hi_z);
+            normals.emplace_back(0.f, 1.f, 0.f);
+            normals.emplace_back(0.f, 1.f, 0.f);
+            normals.emplace_back(0.f, 1.f, 0.f);
+
+            texcoord.emplace_back(hi_x_text_coord, lo_y_text_coord);
+            texcoord.emplace_back(lo_x_text_coord, hi_y_text_coord);
+            texcoord.emplace_back(hi_x_text_coord, hi_y_text_coord);
         }
     }
 
@@ -113,6 +138,14 @@ try {
     output_file.print("{}\n", vertices.size());
     for (auto const& vertex : vertices) {
         output_file.print("{} {} {}\n", vertex.x, vertex.y, vertex.z);
+    }
+    output_file.print("\n");
+    for (auto const& vertex : normals) {
+        output_file.print("{} {} {}\n", vertex.x, vertex.y, vertex.z);
+    }
+    output_file.print("\n");
+    for (auto const& vertex : texcoord) {
+        output_file.print("{} {}\n", vertex.x, vertex.y);
     }
     return {};
 } catch (...) {
